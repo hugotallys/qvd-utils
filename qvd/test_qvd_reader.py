0667b6e0dbac42d91b69eece2a7f69c970ed9b25
hugotallys <htmo@ic.ufal.br>
2023-11-22 11:13:42 -0300

    adds basic chunk load test

--- qvd/test_qvd_reader.py
+++ qvd/test_qvd_reader.py
@@ -6,11 +6,18 @@ import numpy as np
 
 class TestQvdReader():
     def test_read_shape(self):
-        qvd = qvd_reader.read(f'{os.path.dirname(__file__)}/test_files/AAPL.qvd')
-        csv = pd.read_csv(f'{os.path.dirname(__file__)}/test_files/AAPL.csv', float_precision='round_trip')
+        qvd = qvd_reader.read(
+            f'{os.path.dirname(__file__)}/test_files/AAPL.qvd')
+        csv = pd.read_csv(
+            f'{os.path.dirname(__file__)}/test_files/AAPL.csv',
+            float_precision='round_trip')
         assert qvd.shape == csv.shape
 
     def test_read_size(self):
-        qvd = qvd_reader.read(f'{os.path.dirname(__file__)}/test_files/AAPL.qvd')
-        csv = pd.read_csv(f'{os.path.dirname(__file__)}/test_files/AAPL.csv', float_precision='round_trip')
-        assert np.array_equal(np.sort(qvd.columns, axis=0), np.sort(csv.columns, axis=0))
+        qvd = qvd_reader.read(
+            f'{os.path.dirname(__file__)}/test_files/AAPL.qvd')
+        csv = pd.read_csv(
+            f'{os.path.dirname(__file__)}/test_files/AAPL.csv',
+            float_precision='round_trip')
+        assert np.array_equal(
+            np.sort(qvd.columns, axis=0), np.sort(csv.columns, axis=0))
